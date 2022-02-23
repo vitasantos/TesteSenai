4b3a3ff4d4dffbf1bfd5796947c1086026e45c32
Vitoria Santos <vii.santos@hotmail.com>
2022-02-23 17:56:35 -0300

    primeiro commit

---
+++ src/test/java/teste/senai/cursos/TesteBuscaCursos.java
@@ -0,0 +1,31 @@
+package teste.senai.cursos;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteBuscaCursos {
+	private WebDriver driver;
+	
+	@Before
+	public void abrirNavegador() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver(); //abre o navegador
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void testeNavegacao() {
+		driver.get("https://informatica.sp.senai.br/");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
+		//driver.findElement(By.id("oqEstabuscando")).sendKeys("gestão"); 
+		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("gestão");
+		//driver.findElement(By.className("btnBuscaJavaScript")).click();
+		driver.findElement(By.id("Busca1_btnBusca")).click();
+	}
+}
